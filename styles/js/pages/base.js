fbcd449fbefd24506a0ecbbda4453e3fd02e2864
Juni <junikimdraws@gmail.com>
2025-06-03 19:17:05 -0500

    Try having the thumbnails outside

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -8,7 +8,6 @@ import { charadex } from '../utilities.js';
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", () => {
   charadex.tools.loadIncludedFiles();
-  charadex.tools.loadIncludedFiles(); // we only ever go 2 deep...
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
 });
