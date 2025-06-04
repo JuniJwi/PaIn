f173c336c84e59881fb8b4948e3e5e5bd8c7da3c
Juni <junikimdraws@gmail.com>
2025-06-03 19:01:52 -0500

    undo recursive to check html

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -7,13 +7,7 @@ import { charadex } from '../utilities.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", () => {
-  let loadHTML = $(".load-html");
-  console.log("There are ", loadHTML.length, "loadable HTML elements.");
-  while (loadHTML.length > 0) {
-    charadex.tools.loadIncludedFiles();
-    loadHTML = $(".load-html");
-    console.log("There are ", loadHTML.length, "loadable HTML elements.");
-  }
+  charadex.tools.loadIncludedFiles();
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
 });
